22ae5b6f287d884e9b66449fa73f53b038e7cb4b
Woody <SheriffWoody@users.noreply.github.com>
2018-10-25 14:23:39 +0800

    Update push.sh

--- templates/push.sh
+++ templates/push.sh
@@ -14,4 +14,4 @@ git commit -m $VersionString
 git tag ${VersionString}
 git push origin master --tags
 #发布
-pod repo push CYTCocoaPodsSpecs $podspec --allow-warnings --use-libraries --verbose
+pod repo push “yourRepoName” $podspec --allow-warnings --use-libraries --verbose
