e9ae2bfd445165ec91735f9e695ee5c46224109b
woody <dean.hu@drcuiyutao.com>
2019-05-28 14:24:13 +0800

    push脚本

--- templates/push.sh
+++ templates/push.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+# 发布
+function push(){
+	# public pod
+	# pod trunk push $podspec --allow-warnings --use-libraries --verbose
+
+	# private pod
+	# pod repo push “yourRepoName” $podspec --allow-warnings --use-libraries --verbose
+}
+
 # 获取当前脚本所在目录
 cd $(dirname "$0")
 
@@ -8,10 +17,19 @@ podspec="$(ls *.podspec)"
 VersionString=`grep -E 's.version.*=' $podspec|cut -d "\"" -f2|cut -d "\"" -f1|cut -d "'" -f2|cut -d "'" -f1`
 echo $VersionString
 
-git add .
-git commit -m $VersionString
+read -p "提交（1）or 发布（2）：" num
+if [[ $num == 1 ]]; then
+    git add .
+    git commit -m $VersionString
 
-git tag ${VersionString}
-git push origin master --tags
-#发布
-pod repo push “yourRepoName” $podspec --allow-warnings --use-libraries --verbose
+    git tag ${VersionString}
+    git push origin master --tags
+	read -p "是否发布pod（Y/N）：" push
+	if [[ $push == 'Y' || $push == 'y' ]]; then
+		push
+	fi
+else
+	push
+fi
+
+
